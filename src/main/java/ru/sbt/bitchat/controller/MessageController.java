3bc2b11d0344cbcab1417c716257343d9e3f1aea
konstantin1998 <galperin.15@yandex.ru>
2021-03-04 20:35:33 +0300

    get-request

--- src/main/java/ru/sbt/bitchat/controller/MessageController.java
+++ src/main/java/ru/sbt/bitchat/controller/MessageController.java
@@ -1,21 +1,35 @@
 package ru.sbt.bitchat.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 import ru.sbt.bitchat.entity.MessageEntity;
 import ru.sbt.bitchat.repository.MessageRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageRepository messageRepository;
 
-    @GetMapping("")
+    @GetMapping("/messageId")
     public List<MessageEntity> get(@RequestParam long messageId) {
-        return messageRepository.findAll();
+        int n = 20;
+        LocalDateTime timeStamp = messageRepository.findById(messageId).get().getTime();
+        List<MessageEntity> messages = messageRepository
+                .findAll(Sort.by(Sort.Direction.DESC, "time"))
+                .stream()
+                .filter((message) -> message.getTime().compareTo(timeStamp) <= 0)
+                .limit(n)
+                .collect(Collectors.toList());
+        return messages;
+    }
+
+    @PostMapping
+    public long add(@RequestBody MessageEntity entity) {
+        return 0;
     }
 }
--- src/main/java/ru/sbt/bitchat/repository/MessageRepository.java
+++ src/main/java/ru/sbt/bitchat/repository/MessageRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import ru.sbt.bitchat.entity.MessageEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
     List<MessageEntity> findAll();
+    Optional<MessageEntity> findById(Long id);
 }
